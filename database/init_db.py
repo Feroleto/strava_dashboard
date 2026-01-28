88cca05ad42c0a4a1ce2cc254ee8abc4d3244ff6
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-01-28 11:37:05 -0300

    creating activity_seconds db

--- database/init_db.py
+++ database/init_db.py
@@ -1,4 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from database.config import Base, engine 
+from models import Activity, ActivitySplit, ActivitySecond
 
 def create_tables():
     Base.metadata.create_all(engine)
--- database/models.py
+++ database/models.py
@@ -1,5 +1,10 @@
 from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from database.config import Base
 
 class Activity(Base):
