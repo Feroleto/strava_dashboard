6ea63935f17855154e5da3cf597b0a686bc2c833
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-01-28 16:51:59 -0300

    adding other db for tests

--- database/config.py
+++ database/config.py
@@ -1,8 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///strava.db"
+IS_TEST_MODE = os.environ.get("STRAVA_TEST_MODE") == "1"
+DB_NAME = "strava_test.db" if IS_TEST_MODE else "strava.db"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_PATH = os.path.join(BASE_DIR, "..", DB_NAME)
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def get_db_info():
+    mode = "TEST DB" if IS_TEST_MODE else "MAIN DB"
+    return f"Mode: {mode} | DATABASE: {DATABASE_PATH}"
+
+# to use the TEST database
+# $env:STRAVA_TEST_MODE="1"; python seu_script.py
+
+# to use the MAIN database
+# $env:STRAVA_TEST_MODE="0"; python seu_script.py
--- database/init_db.py
+++ database/init_db.py
@@ -2,10 +2,12 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from database.config import Base, engine 
+from database.config import Base, engine, get_db_info 
 from models import Activity, ActivitySplit, ActivitySecond
 
 def create_tables():
+    print(get_db_info())
+    print("-" * 30)
     Base.metadata.create_all(engine)
     print("creating tables")
     
