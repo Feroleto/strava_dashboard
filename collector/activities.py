85acb548d750bb657f6d4599621f482badd842bf
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-01-30 12:11:12 -0300

    cleaning the collector folder

--- collector/activities.py
+++ collector/activities.py
@@ -5,18 +5,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from auth.token_manager import get_valid_access_token
-
-BASE_URL = "https://www.strava.com/api/v3"
-
-def _get_headers():
-    token = get_valid_access_token()
-    return {
-        "Authorization": f"Bearer {token}"
-    }
+from collector.utils import BASE_URL , get_headers
 
 def fetch_activities(per_page = 30, page = 1, after=None):
-    headers = _get_headers()
+    headers = get_headers()
     
     params = {
         "per_page": per_page,
@@ -33,7 +25,6 @@ def fetch_activities(per_page = 30, page = 1, after=None):
     )
     
     response.raise_for_status()
-    
     return response.json()
 
 def get_all_activities(per_page = 200, after=None):
@@ -56,7 +47,7 @@ def get_all_activities(per_page = 200, after=None):
     return all_activities
 
 def get_activity_by_id(activity_id: int, sleep_sec: float = 0.3):
-    headers = _get_headers()
+    headers = get_headers()
     
     response = requests.get(
         f"{BASE_URL}/activities/{activity_id}",
@@ -64,7 +55,5 @@ def get_activity_by_id(activity_id: int, sleep_sec: float = 0.3):
     )
     
     response.raise_for_status()
-    
     time.sleep(sleep_sec)
-    
     return response.json()
--- collector/streams.py
+++ collector/streams.py
@@ -4,17 +4,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from auth.token_manager import get_valid_access_token
+from collector.utils import BASE_URL, get_headers
 
-BASE_URL = "https://www.strava.com/api/v3"
-
-def fetch_activity_streams(activity_id, access_token=None):
-    if not access_token:
-        access_token = get_valid_access_token()
-        
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
+def fetch_activity_streams(activity_id):
+    headers =  get_headers()
     
     params = {
         "keys": "time,distance,velocity_smooth,heartrate,altitude",
