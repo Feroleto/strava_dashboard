bf8470326820150fd5b7617b5b4cb5234400daf0
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-01-30 15:13:43 -0300

    adding delta distance in streams table

--- processors/streams.py
+++ processors/streams.py
@@ -11,12 +11,25 @@ def map_streams_to_db_model(activity_id, streams):
     hr_stream = streams.get("heartrate", {}).get("data", [])
     alt_stream = streams.get("altitude", {}).get("data", [])
     
+    prev_distance = None
+    
     for i in range(len(time_stream)):
+        total_distance = distance_stream[i]
+        
+        if prev_distance is None:
+            delta_distance = 0.0
+        else:
+            delta_distance = total_distance - prev_distance
+            
+        prev_distance = total_distance
+        
         speed = speed_stream[i] if (speed_stream and i < len(speed_stream)) else None
+        
         yield ActivitySecond(
             activity_id=activity_id,
             second_index=time_stream[i],
-            distance_m=distance_stream[i],
+            distance_total_m=total_distance,
+            distance_delta_m=max(delta_distance, 0),
             speed_m_s=speed,
             heart_rate=hr_stream[i] if i < len(hr_stream) else None,
             elevation_m=alt_stream[i] if i < len(alt_stream) else None,
