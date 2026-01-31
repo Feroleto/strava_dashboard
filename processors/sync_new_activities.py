8eef508a274e5cafd025608feae74ead237573d4
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-01-31 10:46:10 -0300

    Update sync_new_activities.py
    optimizing sync time for few activities and adding error treatment for possible api limit request reached

--- processors/sync_new_activities.py
+++ processors/sync_new_activities.py
@@ -29,13 +29,17 @@ def sync_new_activities():
     
     runs_to_process = [s for s in summary_activities if s.get("type") == "Run"]
     
+    base_sleep = 1 if len(runs_to_process) < 45 else 10
+    
     saved_count = 0
     errors_count = 0
+    api_calls = 1
     
     pbar = tqdm(runs_to_process, desc="Starting synchronization", unit="atv", colour="cyan")
     
     for summary in pbar:
-        pbar.set_description(f"Processing: {summary.get("name")[:25]}")
+        pbar.set_description(f"Processing: {summary.get("name")[:20]}")
+        pbar.set_postfix(api_reqs = api_calls, saved = saved_count, errors = errors_count)
         
         exists = (
             session.query(Activity)
@@ -43,38 +47,49 @@ def sync_new_activities():
             .first()
         )
         if exists:
-            pbar.set_postfix(status="Activity already exists")
             continue
         
         try:
             full_data = get_activity_by_id(summary["id"])
+            api_calls += 1
+            
             activity_obj = map_activity_to_db_model(full_data)
             session.add(activity_obj)
             session.flush()
             
             if activity_obj.workout_type in STREAM_WORKOUT_TYPES:
-                pbar.set_postfix(status="Downloading Streams", last_id=summary["id"])
+                pbar.set_postfix(api_reqs = api_calls, status = "Downloading Streams")
                 streams = fetch_activity_streams(activity_obj.id)
+                api_calls += 1
+                
                 streams_obj = map_streams_to_db_model(activity_obj.id, streams)
                 session.add_all(streams_obj)
             else:
-                pbar.set_postfix(status="Downloading Splits", last_id=summary["id"])
+                pbar.set_postfix(api_reqs = api_calls, status = "Downloading Splits")
                 splits_data = full_data.get("splits_metric", [])
                 splits_obj = map_splits_to_db_model(activity_obj.id, splits_data)
                 session.add_all(splits_obj)
             
             session.commit()
             saved_count += 1
-            time.sleep(10)
+            time.sleep(base_sleep)
         
         except Exception as e:
+            if "429" in str(e):
+                pbar.write("API request limit reached, waiting for 15 min")
+                time.sleep(60 * 15)
+                continue
+            
             errors_count += 1
             pbar.write(f"Error in the activity: {summary["id"]}: {e}")
             session.rollback()
+            time.sleep(2)
         
     pbar.colour = "green"
     pbar.set_description("Sync completed")
     pbar.refresh()
+    pbar.close()
+    
     session.close()
     print(f"{saved_count} new activities were saved, {errors_count} errors")
                 
