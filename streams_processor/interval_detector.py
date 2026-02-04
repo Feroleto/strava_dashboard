192c27370601e3f38e1ee07c17f15f4d67a65578
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-02-04 17:10:24 -0300

    removing print parts that now are on intervals_debug folder

--- streams_processor/interval_detector.py
+++ streams_processor/interval_detector.py
@@ -1,24 +1,6 @@
-import math
-
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from database.config import SessionLocal
-from database.models import ActivitySecond
-from streams_pandas_interpolation.processing_streams import process_activity_streams_pd
-
 MIN_SPEED = 3.3
 MAX_BREAK_ALLOWED = 15
 MIN_BLOCK_DIST = 150
-
-#ACTIVITY_ID = 16888423217 # 10x400 -> pd teve melhor precisão, mas diferença foi muito baixa
-#ACTIVITY_ID = 16962023462 # 5x200 + 4x400 + 2x800
-#ACTIVITY_ID = 16848750867 # 5x400 + 1x1km
-#ACTIVITY_ID = 16819680946 # 10x200 + 5x400
-#ACTIVITY_ID = 14392947474 # recorded on strava -> don't find intervals
-#ACTIVITY_ID = 16527840409 # 6 x (200 + 400)
-ACTIVITY_ID = 16279513590 # strava recorded activity -> find intervals
 
 class IntervalDetector:
     def __init__(
@@ -50,7 +32,6 @@ class IntervalDetector:
                     gap_counter += 1
                 elif current_block:
                     real_block = current_block[:-gap_counter] if gap_counter > 0 else current_block
-                    #real_block = current_block
                     
                     if self._is_valid_block(real_block):
                         blocks.append(self._summarize_block(real_block))
@@ -60,7 +41,6 @@ class IntervalDetector:
         
         if current_block:
             real_block = current_block[:-gap_counter] if gap_counter > 0 else current_block
-            #real_block = current_block
             if self._is_valid_block(real_block):
                 blocks.append(self._summarize_block(real_block))
                 
@@ -91,44 +71,3 @@ class IntervalDetector:
             "avg_pace": pace_seconds,
             "avg_hr": sum((d[1].get("heart_rate") or 0.0) for d in block) / len(block)
         }
-        
-def format_pace(seconds):
-    if not seconds or math.isinf(seconds): return "0:00"
-    m, s = divmod(int(seconds), 60)
-    return f"{m:02d}:{s:02d}"
-
-def run_interval_analysis(processed_activity):
-    detector = IntervalDetector(min_speed=MIN_SPEED, min_block_dist=MIN_BLOCK_DIST)
-    intervals = detector.detect_intervals(processed_activity)
-    
-    print(f"\n" + "="*80)
-    print(f" DETECTED SPLITS ANALYSIS: {len(intervals)} splits")
-    print("-" * 80)
-    print(f"{'Split':<6} | {'Start':<7} | {'Time':<8} | {'Distance':<10} | {'Pace'}")
-    print("-" * 80)
-    
-    for i, interval in enumerate(intervals, 1):
-        print(f"{i:<6} | {interval['start_sec']:>5}s | {interval['duration_sec']:>6}s | "
-              f"{interval['distance_m']:8.1f}m | {format_pace(interval['avg_pace'])}")
-        
-        #print("="*80)
-        
-def get_streams():
-    session = SessionLocal()
-    
-    seconds = (
-        session.query(ActivitySecond)
-        .filter_by(activity_id=ACTIVITY_ID)
-        .order_by(ActivitySecond.second_index)
-        .all()
-    )
-    
-    return seconds
-        
-def main():
-    streams = get_streams()
-    processed_streams = process_activity_streams_pd(streams)
-    run_interval_analysis(processed_streams)
-    
-if __name__ == "__main__":
-    main()
