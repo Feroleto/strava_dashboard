c5eadab4cde6918dafea9e06f000a273d48765b7
Guilherme Feroleto <feroletoguilherme@gmail.com>
2026-02-07 12:45:59 -0300

    Update laps_extractor.py
    adding necessary field on the laps recorded from the watch

--- intervals_processor/laps_extractor.py
+++ intervals_processor/laps_extractor.py
@@ -1,23 +1,30 @@
-def extract_laps_from_activities(full_activity_data):
+def extract_recorded_laps(full_activity_data):
     laps = full_activity_data.get("laps", [])
     extracted_laps = []
     
-    for lap in laps:
+    for i, lap in enumerate(laps):
+        avg_speed = lap.get("average_speed", 0)
+        avg_pace = 1000 / avg_speed if avg_speed > 0.3 else 0
+        distance = lap.get("distance", 0)
+        duration = lap.get("moving_time", 0)
+        elev_gain = float(lap.get("total_elevation_gain", 0))
+
+        grade = (elev_gain / distance * 100) if distance > 0 else 0
+        vam = (elev_gain / duration * 3600) if duration > 0 else 0        
+                    
         extracted_laps.append({
-            "name": lap.get("name"),
-            "lap_index": lap.get("lap_index"),
-            "start_index": lap.get("start_index"),
-            "end_index": lap.get("end_index"),
-            "distance": lap.get("distance"),
-            "moving_time": lap.get("moving_time"),
-            "avg_speed": lap.get("average_speed"),
-            "pace": 1000 / lap.get("average_speed") if lap.get("average_speed") > 0 else 0
+            "type": str(lap.get("name", f"LAP {i+1}")),
+            "lap_index": lap.get("lap_index", i+1),
+            "start_sec": lap.get("start_index", 0),
+            "end_sec": lap.get("end_index", 0),
+            "distance_m": round(float(distance), 1),
+            "total_duration_sec": int(lap.get("elapsed_time", duration)),
+            "moving_duration_sec": int(duration),
+            "avg_pace": avg_pace,
+            "avg_hr": round(float(lap.get("average_heartrate", 0)), 1),
+            "elev_gain_m": round(elev_gain, 1),
+            "avg_grade_percent": round(grade, 1),
+            "vam": round(vam, 0)
         })
         
     return extracted_laps
-
-# filter laps with 5:00 or lower pace and distance > 100
-def filter_speed_laps(all_activity_laps):
-    splits = [l for l in all_activity_laps if l["pace"] < 300 and l["distance"] > 100]
-    
-    return splits
